67e75c0d952dcbaf4ae2e4be10e0adab74b9a9b4
Anton <antonogonovskiy@gmail.com>
2022-08-20 20:49:14 +0300

    feat: add api requests

--- src/api/api.tsx
+++
@@ -1,5 +0,0 @@
-export async function getWords() {
-  const response = await fetch('https://react-learnwords-rslangg.herokuapp.com/words')
-  const data = await (response.json())
-  console.log(data)
-}
