79d86bdf701762ca28eb10b34c468581d8a11cdb
Yury Ahanouski <uraaru@live.com>
2022-09-03 20:12:52 +0300

    feat: add full main page

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,17 +1,17 @@
-import { Switch } from "@mui/material";
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Games } from "../pages/games";
 import { Main } from "../pages/main";
 import { Tutorial } from "../pages/tutorial";
+import Statistics from "./Statistics";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
         <Route path="/games" element={<Games />} />
         <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Routes>
     </BrowserRouter>
   );
