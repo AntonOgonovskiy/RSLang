66413a42b81e00b9d33c884c0f33180e2ccb5558
Yury Ahanouski <uraaru@live.com>
2022-09-03 20:12:52 +0300

    feat: add full main page

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+export const creators = [
+  {
+    link: "https://github.com/AntonOgonovskiy",
+    nick: "@AntonOgonovskiy",
+    badges: ["teamlead", "учебник", "api", "слова"],
+    colors: ["#f44336", "#9c27b0", "#ffc107", "#cddc39"],
+    img: "creators/AntonOgonovski.png",
+  },
+  {
+    link: "https://github.com/Kaelns",
+    nick: "@Kaelns",
+    badges: ["главная", "авторизация", "статистика"],
+    colors: ["#fb8c00", "#8bc34a", "#ff5722"],
+    img: "creators/Kaelns.jpg",
+  },
+  {
+    link: "https://github.com/NikitaLemesh",
+    nick: "@NikitaLemesh",
+    badges: ["аудиовызов", "спринт"],
+    colors: ["#ffc107", "#cddc39"],
+    img: "creators/NikitaLemesh.png",
+  },
+];
